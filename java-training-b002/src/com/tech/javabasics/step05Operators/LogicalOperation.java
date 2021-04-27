5235e1689e02d4db5300bb42b905552b109cd2c6
vlearn-tech <vlearn.tech.official@gmail.com>
2021-04-27 10:11:57 +0530

    Operators, Loops

---
+++ java-training-b002/src/com/tech/javabasics/step05Operators/LogicalOperation.java
@@ -0,0 +1,22 @@
+package com.tech.javabasics.step05Operators;
+
+public class LogicalOperation {
+	public static void main(String[] args) {
+		int age = 10 + (int) (Math.random() * 40);
+		String[] names = { "Ashish", "Mohit", "Sudha", "Madhu", "Sachin" };
+		String name = names[(int) (Math.random() * names.length)];
+
+		System.out.println(String.format("Chosen person is %s and his/her age is %d", name, age));
+
+		int count = 0;
+
+		if (name.startsWith("M") & age >= 25 & count++ >= 0) {
+			System.out.println("You have won a lottery");
+		}
+		System.out.println(count);
+
+		// && - more efficient - quick exit
+		// & - evaluates each and every condition even if it knows the outcome beforehand
+
+	}
+}
