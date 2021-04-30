2dbce745eb580e49c8ae40c768445a736231e32a
vlearn-tech <vlearn.tech.official@gmail.com>
2021-04-30 09:29:51 +0530

    Loops, Strings

---
+++ java-training-b002/src/com/tech/javabasics/step07Strings/StringBasics.java
@@ -0,0 +1,11 @@
+package com.tech.javabasics.step07Strings;
+
+public class StringBasics {
+	public static void main(String[] args) {
+		String name = "Vikas";
+		char[] nameAsCharArray = { 'V', 'i', 'k', 'a', 's' };
+
+		System.out.println(name);
+		System.out.println(nameAsCharArray);
+	}
+}
